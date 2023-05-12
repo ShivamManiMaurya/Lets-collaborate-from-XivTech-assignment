69d6d5e7c6f298753c2e80a07c9e540cbd5a3c1e
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2023-05-12 23:48:30 +0530

    desktop-view media query done

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,45 +1,84 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import arrowImg from "../images/Arrow-black.svg";
 import "../Components/Main.css";
 import { useInView } from "react-intersection-observer";
 
 function Main({ heading, image, description, linkText }) {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
     const { ref: myRef, inView: componentIsVisible } = useInView();
 
-    // if (componentIsVisible) {
-    //     console.log("visible");
-    // } else {
-    //     console.log("not visible");
-    // }
+    useEffect(() => {
+        function handleResize() {
+            setWindowWidth(window.innerWidth);
+        }
 
-    // const myRef = useRef();
-
-    // useEffect(() => {
-    //     console.log("myref = ", myRef.current);
-    // }, []);
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
 
     return (
         <div
             className={`main ${componentIsVisible && "scroll-animation"}`}
             ref={componentIsVisible ? null : myRef}
         >
-            <h2 className="main-heading-h2">{heading}</h2>
-            <img
-                className="main-img"
-                src={image}
-                alt={`${heading}-service-image`}
-            />
-            <h3 className="main-description-h3">{description}</h3>
-            <div className="main-link-div">
-                <a className="main-link-a" href="#">
-                    <p className="main-linkText-p">{linkText}</p>
-                    <img
-                        className="main-arrow-img"
-                        src={arrowImg}
-                        alt="black-arrow"
-                    />
-                </a>
-            </div>
+            {windowWidth < 778 ? (
+                <div>
+                    {" "}
+                    <div className="main-heading-div">
+                        <h2 className="main-heading-h2">{heading}</h2>
+                    </div>
+                    <div className="main-img-div">
+                        <img
+                            className="main-img"
+                            src={image}
+                            alt={`${heading}-service-img`}
+                        />
+                    </div>
+                    <div className="main-bottom-div">
+                        <h3 className="main-description-h3">{description}</h3>
+
+                        <div className="main-link-div">
+                            <a className="main-link-a" href="#">
+                                <p className="main-linkText-p">{linkText}</p>
+                                <img
+                                    className="main-arrow-img"
+                                    src={arrowImg}
+                                    alt="black-arrow"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <div className="main-large">
+                    <div>
+                        <h2 className="main-heading-h2">{heading}</h2>
+                        <div className="main-large-description-div">
+                            <h3 className="main-description-h3">
+                                {description}
+                            </h3>
+                        </div>
+                        <div className="main-link-div">
+                            <a className="main-link-a" href="#">
+                                <p className="main-linkText-p">{linkText}</p>
+                                <img
+                                    className="main-arrow-img"
+                                    src={arrowImg}
+                                    alt="black-arrow"
+                                />
+                            </a>
+                        </div>
+                    </div>
+                    <div className="main-img-div">
+                        <img
+                            className="main-img"
+                            src={image}
+                            alt={`${heading}-service-img`}
+                        />
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
